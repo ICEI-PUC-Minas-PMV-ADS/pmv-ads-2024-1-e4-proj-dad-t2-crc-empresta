2f38f39452e1cd5a57648685408ff5791270ad9d
Claudio Barroso <115187389+ClaudioBarroso@users.noreply.github.com>
2024-04-07 19:51:31 -0300

    Models Category

---
+++ src/BackendApi/ApiCrcEmpresta/Models/Category.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ApiCrcEmpresta.Models;
+{
+  public class Category
+  {
+      [BsonId]
+      [BsonRepresentation(BsonType.ObjectId)]
+      public string Id { get; set; }
+      
+      public string Name { get; set; }
+      
+      public List<Item> Items { get; set; } = new List<Item>();
+  }
+}
