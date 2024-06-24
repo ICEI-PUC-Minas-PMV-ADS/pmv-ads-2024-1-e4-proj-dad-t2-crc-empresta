8030dd883fc65ae18a719f3d41bb683c56ddb2e6
Jansem <jansemwall@gmail.com>
2024-06-23 23:05:15 -0300

    refactor: Update admin page component to use PascalCase naming convention

--- src/web/app/(pages)/admin/page.tsx
+++ src/web/app/(pages)/admin/page.tsx
@@ -5,22 +5,22 @@ import Item from "./comp/item";
 import { useState } from 'react';
 import { Tabs } from '@mantine/core';
 
-export default function admin(){
-    const [activeTab, setActiveTab] = useState<string | null>('first');
-return (
+export default function Admin() {
+  const [activeTab, setActiveTab] = useState<string | null>('first');
+  return (
     <>
-    
-   <Tabs value={activeTab} onChange={setActiveTab}>
-      <Tabs.List>
-        <Tabs.Tab value="first">Usuários</Tabs.Tab>
-        <Tabs.Tab value="second">Itens</Tabs.Tab>
-        <Tabs.Tab value="Terceiro">Categoria</Tabs.Tab>
-      </Tabs.List>
 
-      <Tabs.Panel value="first"><User/></Tabs.Panel>
-      <Tabs.Panel value="second"><Item/></Tabs.Panel>
-      <Tabs.Panel value="Terceiro"><Category/></Tabs.Panel>
-    </Tabs>
+      <Tabs value={activeTab} onChange={setActiveTab}>
+        <Tabs.List>
+          <Tabs.Tab value="first">Usuários</Tabs.Tab>
+          <Tabs.Tab value="second">Itens</Tabs.Tab>
+          <Tabs.Tab value="Terceiro">Categoria</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="first"><User /></Tabs.Panel>
+        <Tabs.Panel value="second"><Item /></Tabs.Panel>
+        <Tabs.Panel value="Terceiro"><Category /></Tabs.Panel>
+      </Tabs>
     </>
-)
+  )
 }
